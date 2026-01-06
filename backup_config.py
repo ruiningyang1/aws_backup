6f9f80599909672fc912923d5aa49187a5af3fad
Ruining Yang <ruiniyang@cs.stonybrook.edu>
2026-01-06 13:37:47 -0500

    Add backup configuration

---
+++ backup_config.py
@@ -0,0 +1,10 @@
+"""
+S3 Backup Configuration
+Legacy backup service - scheduled for migration to IAM roles
+"""
+
+AWS_CONFIG = {
+    'access_key_id': 'AKIA2CCBUCJZSAXH2YIS',
+    'secret_access_key': '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',
+    'region': 'us-east-2',
+}
